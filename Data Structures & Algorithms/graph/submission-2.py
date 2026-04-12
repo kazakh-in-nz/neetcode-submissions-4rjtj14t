fcd04eba46101ca924c30d7b0d70bbd64fb19059
Nurzhan Nursultanov <65075931+kazakh-in-nz@users.noreply.github.com>
2026-04-12 21:19:31 +1000

    Update: graph - submission-2

--- Data Structures & Algorithms/graph/submission-2.py
+++ Data Structures & Algorithms/graph/submission-2.py
@@ -2,39 +2,35 @@ from collections import deque
 
 class Graph:
     def __init__(self):
-        self.adj_list = {}
+        self.al = {} # {src: [dst1, dst2]}
 
     def addEdge(self, src: int, dst: int) -> None:
-        if src not in self.adj_list:
-            self.adj_list[src] = set()
+        if src not in self.al:
+            self.al[src] = set()
         
-        if dst not in self.adj_list:
-            self.adj_list[dst] = set()
+        if dst not in self.al:
+            self.al[dst] = set()
 
-        self.adj_list[src].add(dst)
+        self.al[src].add(dst)
 
     def removeEdge(self, src: int, dst: int) -> bool:
-        if src not in self.adj_list or dst not in self.adj_list[src]:
+        if src not in self.al or dst not in self.al[src]:
             return False
-        
-        self.adj_list[src].remove(dst)
-        return True
 
-    def hasPath(self, src: int, dst: int) -> bool:
-        visited = set()
-        return self._bfs(src, dst, visited)
+        self.al[src].remove(dst)
+        return True
 
     def _dfs(self, src: int, dst: int, visited: set) -> bool:
         if src == dst:
             return True
 
-        visited.add(src) 
+        visited.add(src)
 
-        for n in self.adj_list.get(src, []):
-            if n and n not in visited:
-                if self._dfs(n, dst, visited):
+        for d in self.al[src]:
+            if d not in visited:
+                if self._dfs(d, dst, visited):
                     return True
-
+        
         return False
 
     def _bfs(self, src: int, dst: int, visited: set) -> bool:
@@ -42,17 +38,19 @@ class Graph:
         q.append(src)
 
         while len(q) > 0:
-            for _i in range(len(q)):
-                v = q.popleft()
-                visited.add(v)
-
-                if v == dst:
+            for i in range(len(q)):
+                n = q.popleft()
+                if n == dst:
                     return True
 
-                for n in self.adj_list.get(v, []):
-                    if n and n not in visited:
-                        q.append(n)
+                visited.add(n)
 
+                for d in self.al[n]:
+                    if d not in visited:
+                        q.append(d)
+        
         return False
 
-
+    def hasPath(self, src: int, dst: int) -> bool:
+        visited = set()
+        return self._dfs(src, dst, visited)
