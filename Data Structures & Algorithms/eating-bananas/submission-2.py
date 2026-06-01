f8896c747786386c498fb3a36982f7b9853bbb33
Nurzhan Nursultanov <65075931+kazakh-in-nz@users.noreply.github.com>
2026-06-01 21:53:29 +1000

    Add: eating-bananas - submission-2

---
+++ Data Structures & Algorithms/eating-bananas/submission-2.py
@@ -0,0 +1,25 @@
+class Solution:
+    def _eat(self, piles: List[int], speed: int) -> int:
+        hours = 0
+
+        for p in piles:
+            hours += math.ceil(p/speed)
+
+        return hours
+
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        low, high = 1, max(piles)
+
+        while low <= high:
+            mid = low + (high - low) // 2
+
+            h_temp = self._eat(piles, mid)
+            print(h_temp, mid)
+
+            if h_temp <= h:
+                high = mid - 1
+            else:
+                low = mid + 1
+
+        return low
+        
