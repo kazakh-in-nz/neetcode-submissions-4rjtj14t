add24321c31e50f8ce3d2e590409ba632ff253c0
Nurzhan Nursultanov <65075931+kazakh-in-nz@users.noreply.github.com>
2026-07-29 20:33:03 +1000

    Add: maximum-average-subtree - submission-0

---
+++ Data Structures & Algorithms/maximum-average-subtree/submission-0.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def maximumAverageSubtree(self, root: Optional[TreeNode]) -> float:
+        res = -float("inf")
+
+        def dfs(n: Optional[TreeNode]):
+            nonlocal res
+            if not n:
+                return (0, 0)
+
+            l_ave, l_nodes = dfs(n.left)
+            r_ave, r_nodes = dfs(n.right)
+
+            total_nodes = l_nodes + r_nodes + 1
+            total_sum = l_ave * l_nodes + n.val + r_ave * r_nodes
+            new_avg = total_sum / total_nodes
+
+            res = max(res, new_avg, l_ave, r_ave)        
+
+            return (new_avg, total_nodes)
+
+        dfs(root)
+        return res
