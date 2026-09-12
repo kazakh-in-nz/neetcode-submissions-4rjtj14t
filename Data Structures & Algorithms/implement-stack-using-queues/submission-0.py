3829a72147e957db009acf5e9b88fa94380ed3e3
Nurzhan Nursultanov <65075931+kazakh-in-nz@users.noreply.github.com>
2026-09-12 20:35:36 +1000

    Add: implement-stack-using-queues - submission-0

---
+++ Data Structures & Algorithms/implement-stack-using-queues/submission-0.py
@@ -0,0 +1,31 @@
+from collections import deque
+
+class MyStack:
+    def __init__(self):
+        self.helper = deque([])
+        self.main = deque([])
+
+    def push(self, x: int) -> None:
+        while self.main:
+            self.helper.append(self.main.popleft())
+
+        self.main.append(x)
+        while self.helper:
+            self.main.append(self.helper.popleft())
+
+    def pop(self) -> int:
+        return self.main.popleft()
+
+    def top(self) -> int:
+        return self.main[0]
+        
+    def empty(self) -> bool:
+        return len(self.main) == 0
+
+
+# Your MyStack object will be instantiated and called as such:
+# obj = MyStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.empty()
